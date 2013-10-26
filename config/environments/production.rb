287125918f6cf346e12abb43f17ffd5801230932
Cole <cole@studiofathom.com>
2013-10-26 12:14:59 -0700

    users with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Labelmaker::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  #Note to set this to actual host name
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
