9455930f21542f60d2d78339d5fc9d11be0dafe9
Cole <cole@studiofathom.com>
2013-10-26 10:33:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Labelmaker::Application.config.secret_key_base = '6d65379ebfa6d95e9b269f3b4bd549a4988c69622b0c676ce8ef3f91f6ce4c4a94180899c31b0bca8941c5a14aeffc06c626df9a94a2ab6fad429cc65f8c589c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Labelmaker::Application.config.session_store :cookie_store, key: '_labelmaker_session'
